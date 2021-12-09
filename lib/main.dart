31b82ec0588b1af7482076e46ef99947b73c5d45
sekercih <hkkskrc@gmail.com>
2021-12-09 19:25:34 +0300

    the end

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -21,59 +24,208 @@ class FalciApp extends StatelessWidget {
   }
 }
 
-class FalciSayfasi extends StatelessWidget {
+class FalciSayfasi extends StatefulWidget {
+  @override
+  State<FalciSayfasi> createState() => _FalciSayfasiState();
+}
+
+class _FalciSayfasiState extends State<FalciSayfasi> {
+  int ask = 1;
+
+  int para = 1;
+  int tavsiye = 1;
+
+  int cevap = 0;
+
+  List<String> askDurumu = [
+    'TIKLA GELSİN',
+    'AŞK: Bugün, aşk ve ilişki hayatınızda biraz daha temkinli olmalısın. Onu her konuda doğru anlamda anladığından da emin misin?',
+    'AŞK: Aşk ve ilişki hayatınızda hakimiyeti eline almak ve birlikteliğinizi dilediğiniz gibi yönlendirmek isteyebilirsin. Bu isteğinin denge içerisindeki bir ilişkiye ne şekilde fayda edeceğini gözden geçirmelisin.',
+    'AŞK: Duygularınızın bugün biraz düzensiz olduğunu görebilirsiniz. Güne, daha önce hiç yapmadığınız bir şekilde başlamanızı sağlayan doğal bir elektrik hissi var. Kalbiniz her zaman doğruyu söyler.',
+    'AŞK: Duygularınız çoğu zaman rasyonel düşüncelerinize galip geliyor ve sonunda derinlerde duyguların beslediği bir karmaşa ortaya çıkıyor. Çok da mantıklı düşünerek kalbinizi görmezden gelmeyin. Amacınız bu ikisi arasında dengeyi bulmak.',
+    'AŞK: Duygusal olarak kötü hissetmeyin, sezgileriniz güçlü durumda. Sevgilinizin hislerini bu sezgiler ile anlamaya çalışın. Bazen sadece eğlenmek gerekir, siz de rahatlayın ve birlikte eğlenin!',
+  ];
+  List<String> paraDurumu = [
+    'TIKLA GELSİN',
+    'PARA: Kafanıza uzun süredir koyduğunuz bazı harcamalar var, bugün kendinize bunlara gerçekten ne kadar ihtiyacınız olduğunu sorun ve bir kısmını iptal edin. Bugün bir hayır kurumuna küçücük bir bağış yapın, kendinizi çok daha iyi hissedeceksiniz.',
+    'PARA: Nakit akışınızın dengesini bozabilecek dönemlere giriyoruz, bu aralar kesinlikle masa başına oturup bir bütçe hesabı yapmalısınız, önümüzdeki 3 ay boyunca gelir gider dengenizden emin olmadan hareket etmeyin',
+    'PARA: Bugün, başkalarına yardım, destek ve hizmet duygunuzun yüksek olduğu bir gün olabilir, hayır, bağış işlerinde yer alabilirsiniz.',
+    'PARA: Bugün kendinize güveniniz oldukça yüksek, ancak bu size pek fayda getirmeyebilir, bağlantılarınız size verdikleri destekten şüphelenebilirler. Sakin hareket edin, bir adım geri durun, ve paradan çok sağlığınızla ilgilenin.',
+    'PARA: Para kaynaklarımız ile ilgili detaylar bu dönem dikkat etmelisiniz. Ayrıca maddi konularda ve harcamalarda uzun süredir sizi meşgul eden bir konuda karara varabilirsiniz, yakınlarınızdan veya eşinizden karar alırken yardım isteyin.'
+  ];
+  List<String> tavsiyeDurumu = [
+    'TIKLA GELSİN',
+    'TAVSİYE: Bugün meraklı kişiliğini ön plana çıkartarak insanlara soru sormaktan çekinme ',
+    'TAVSİYE: Bugün daha önce hiç fark etmediğin şeylerin hep orada olduğunu fark edebilirsin, sadece biraz daha dikkatli olmaya çalış',
+    'TAVSİYE: Bugün ikili ilişkilerinde daha aktif ve verici olmaya çalışabilirsin, aynı fikirde olduğun insanları daha dikkatli dinlemeye ve sorgulamaya başla',
+    'TAVSİYE: Bugün sadece işine odaklan, dikkatini dağıtacak her türlü nesne ve sosyal medyadan uzaklaşmak kendini daha mutlu hissettirebilir',
+    'TAVSİYE: Bugün tek başına biraz yürüyüş yap, çocukluğunda dinlediğin şarkıları aç ve o zamanlar oynadığın oyunları hayal et'
+  ];
+
+  List<String> yanitlar = [
+    'TIKLA FALIN GELSİN',
+    'AŞK: Bugün, aşk ve ilişki hayatınızda biraz daha temkinli olmalısın. Onu her konuda doğru anlamda anladığından da emin misin?',
+    'AŞK: Aşk ve ilişki hayatınızda hakimiyeti eline almak ve birlikteliğinizi dilediğiniz gibi yönlendirmek isteyebilirsin. Bu isteğinin denge içerisindeki bir ilişkiye ne şekilde fayda edeceğini gözden geçirmelisin.',
+    'AŞK: Duygularınızın bugün biraz düzensiz olduğunu görebilirsiniz. Güne, daha önce hiç yapmadığınız bir şekilde başlamanızı sağlayan doğal bir elektrik hissi var. Kalbiniz her zaman doğruyu söyler.',
+    'AŞK: Duygularınız çoğu zaman rasyonel düşüncelerinize galip geliyor ve sonunda derinlerde duyguların beslediği bir karmaşa ortaya çıkıyor. Çok da mantıklı düşünerek kalbinizi görmezden gelmeyin. Amacınız bu ikisi arasında dengeyi bulmak.',
+    'AŞK: Duygusal olarak kötü hissetmeyin, sezgileriniz güçlü durumda. Sevgilinizin hislerini bu sezgiler ile anlamaya çalışın. Bazen sadece eğlenmek gerekir, siz de rahatlayın ve birlikte eğlenin!',
+    'PARA: Kafanıza uzun süredir koyduğunuz bazı harcamalar var, bugün kendinize bunlara gerçekten ne kadar ihtiyacınız olduğunu sorun ve bir kısmını iptal edin. Bugün bir hayır kurumuna küçücük bir bağış yapın, kendinizi çok daha iyi hissedeceksiniz.',
+    'PARA: Nakit akışınızın dengesini bozabilecek dönemlere giriyoruz, bu aralar kesinlikle masa başına oturup bir bütçe hesabı yapmalısınız, önümüzdeki 3 ay boyunca gelir gider dengenizden emin olmadan hareket etmeyin',
+    'PARA: Bugün, başkalarına yardım, destek ve hizmet duygunuzun yüksek olduğu bir gün olabilir, hayır, bağış işlerinde yer alabilirsiniz.',
+    'PARA: Bugün kendinize güveniniz oldukça yüksek, ancak bu size pek fayda getirmeyebilir, bağlantılarınız size verdikleri destekten şüphelenebilirler. Sakin hareket edin, bir adım geri durun, ve paradan çok sağlığınızla ilgilenin.',
+    'PARA: Para kaynaklarımız ile ilgili detaylar bu dönem dikkat etmelisiniz. Ayrıca maddi konularda ve harcamalarda uzun süredir sizi meşgul eden bir konuda karara varabilirsiniz, yakınlarınızdan veya eşinizden karar alırken yardım isteyin.',
+    'TAVSİYE: Bugün meraklı kişiliğini ön plana çıkartarak insanlara soru sormaktan çekinme ',
+    'TAVSİYE: Bugün daha önce hiç fark etmediğin şeylerin hep orada olduğunu fark edebilirsin, sadece biraz daha dikkatli olmaya çalış',
+    'TAVSİYE: Bugün ikili ilişkilerinde daha aktif ve verici olmaya çalışabilirsin, aynı fikirde olduğun insanları daha dikkatli dinlemeye ve sorgulamaya başla',
+    'TAVSİYE: Bugün sadece işine odaklan, dikkatini dağıtacak her türlü nesne ve sosyal medyadan uzaklaşmak kendini daha mutlu hissettirebilir',
+    'TAVSİYE: Bugün tek başına biraz yürüyüş yap, çocukluğunda dinlediğin şarkıları aç ve o zamanlar oynadığın oyunları hayal et'
+  ];
+  void durumlarDegistir() {
+    setState(() {
+      cevap = Random().nextInt(5) + 1;
+      yanitlar[cevap];
+      print(yanitlar[cevap]);
+    });
+  }
+
+  void durumlarDegistir1() {
+    setState(() {
+      cevap = 5 + Random().nextInt(5) + 1;
+      yanitlar[cevap];
+      print(cevap);
+    });
+  }
+
+  void durumlarDegistir2() {
+    setState(() {
+      cevap = 10 + Random().nextInt(5) + 1;
+      yanitlar[cevap];
+      print(yanitlar[cevap]);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Container(
-                width: 200,
-                height: 200,
-                child: Image.asset("assets/falci.png")),
-          ),
-          const SizedBox(
-            height: 20,
-            width: 200,
-          ),
-          const Card(
-            margin: EdgeInsets.symmetric(horizontal: 45.0),
-            color: Colors.white70,
-            child: ListTile(
-              leading: Icon(Icons.favorite_outline_outlined, color: Colors.red),
-              title: Text("AŞK DURUMU",
-                  style: TextStyle(fontSize: 10, color: Colors.red)),
+      child: Container(
+        color: Colors.purple[200],
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(6.0),
+              child: Container(
+                width: 150.0,
+                height: 150,
+                child: CircleAvatar(
+                  backgroundColor: Colors.deepPurple[200],
+                  child: ClipRRect(
+                    child: Image(
+                      image: AssetImage('assets/falci.png'),
+                    ),
+                  ),
+                ),
+              ),
             ),
-          ),
-          SizedBox(
-            width: 200,
-          ),
-          const Card(
-            margin: EdgeInsets.symmetric(horizontal: 45.0),
-            color: Colors.white70,
-            child: ListTile(
-              leading: Icon(Icons.shopping_cart, color: Colors.green),
-              title: Text("PARA DURUMU",
-                  style: TextStyle(fontSize: 10, color: Colors.red)),
+            Divider(
+              height: 5,
             ),
-          ),
-          const Card(
-            margin: EdgeInsets.symmetric(horizontal: 45.0),
-            color: Colors.white70,
-            child: ListTile(
-              leading: Icon(Icons.record_voice_over, color: Colors.red),
-              title: Text("GUNLUK TAVSİYE",
-                  style: TextStyle(fontSize: 10, color: Colors.red)),
+            TextButton(
+              style: TextButton.styleFrom(primary: Colors.black),
+              onPressed: durumlarDegistir,
+              child: const Card(
+                margin: EdgeInsets.symmetric(horizontal: 45.0),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(Icons.favorite, color: Colors.red),
+                  title: Text("AŞK DURUMU",
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.red,
+                        shadows: [
+                          Shadow(
+                            blurRadius: 20.0,
+                            color: Colors.red,
+                            offset: Offset(4.0, 4.0),
+                          ),
+                        ],
+                      )),
+                ),
+              ),
             ),
-          )
-        ],
+            Divider(
+              height: 5,
+            ),
+            TextButton(
+              onPressed: durumlarDegistir1,
+              child: const Card(
+                margin: EdgeInsets.symmetric(horizontal: 45.0),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(Icons.shopping_cart, color: Colors.green),
+                  title: Text("PARA DURUMU",
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.red,
+                        shadows: [
+                          Shadow(
+                            blurRadius: 20.0,
+                            color: Colors.red,
+                            offset: Offset(4.0, 4.0),
+                          ),
+                        ],
+                      )),
+                ),
+              ),
+            ),
+            Divider(
+              height: 5,
+            ),
+            TextButton(
+              onPressed: durumlarDegistir2,
+              child: const Card(
+                margin: EdgeInsets.symmetric(horizontal: 45.0),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(Icons.record_voice_over, color: Colors.red),
+                  title: Text("GUNLUK TAVSİYE",
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontStyle: FontStyle.italic,
+                        color: Colors.red,
+                        shadows: [
+                          Shadow(
+                            blurRadius: 20.0,
+                            color: Colors.red,
+                            offset: Offset(4.0, 4.0),
+                          ),
+                        ],
+                      )),
+                ),
+              ),
+            ),
+            Divider(
+              height: 5,
+            ),
+            Center(
+              child: Container(
+                width: 250,
+                child: Text(
+                  yanitlar[cevap],
+                  style: TextStyle(
+                    fontSize: 12,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+
 
 
 
